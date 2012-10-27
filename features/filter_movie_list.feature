baa2c3a160ce5e5aabfec0c0e1b731a043c69115
Eduardo Bortoluzzi Jr <sombrabr@gmail.com>
2012-10-27 12:09:05 -0700

    HOMEWORK 3: BEHAVIOR-DRIVEN DESIGN HAPPY PATHS FOR FILTERING MOVIES & HAPPY PATHS FOR SORTING MOVIES BY TITLE AND BY RELEASE DATE

--- features/filter_movie_list.feature
+++ features/filter_movie_list.feature
@@ -23,13 +23,20 @@ Background: movies have been added to database
   
 Scenario: restrict to movies with 'PG' or 'R' ratings
   # enter step(s) to check the 'PG' and 'R' checkboxes
+  Given I check the following ratings: PG, R
   # enter step(s) to uncheck all other checkboxes
+  And   I uncheck the following ratings: G, PG-13, NC-17
   # enter step to "submit" the search form on the homepage
+  When  I press "Refresh"
   # enter step(s) to ensure that PG and R movies are visible
+  Then  I should see /^(PG|R)$/ in "movies"
   # enter step(s) to ensure that other movies are not visible
+  And   I should not see /^(G|PG-13|NC-17)$/ in "movies"
 
 Scenario: no ratings selected
   # see assignment
 
 Scenario: all ratings selected
-  # see assignment
+  Given I check the following ratings: PG, R, G, PG-13
+  When  I press "Refresh"
+  Then  I should see all of the movies
--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -2,7 +2,7 @@
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
-    Movie.create(movie)
+    Movie.find_or_create_by_title(movie)
   end
 end
 
@@ -12,7 +12,7 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.content  is the entire content of the page as a string.
-  flunk "Unimplemented"
+  page.body =~ /#{Regexp.quote(e1)}.*#{Regexp.quote(e2)}/
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -23,4 +23,39 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  ratings = rating_list.split(',')
+  ratings.each do |rating|
+    step "I #{uncheck}check \"ratings_#{rating.strip}\"" 
+  end
+end
+
+
+Then /^(?:|I )should see \/([^\/]*)\/ in "([^"]*)"$/ do |regexp, table|
+  regexp = Regexp.new(regexp)
+
+  if page.respond_to? :should
+    page.should have_xpath("//table[@id='#{table}']/tbody/tr/td[2]", :text => regexp)
+  else
+    assert page.has_xpath?("//table[@id='#{table}']/tbody/tr/td[2]", :text => regexp)
+  end
+end
+
+Then /^(?:|I )should not see \/([^\/]*)\/ in "([^"]*)"$/ do |regexp, table|
+  regexp = Regexp.new(regexp)
+
+  if page.respond_to? :should
+    page.should have_no_xpath("//table[@id='#{table}']/tbody/tr/td[2]", :text => regexp)
+  else
+    assert page.has_no_xpath?("//table[@id='#{table}']/tbody/tr/td[2]", :text => regexp)
+  end
+end
+
+Then /^I should see all of the movies$/ do
+  total = Movie.count('title')
+  rows = page.all(:xpath, "//table[@id='movies']/tbody/tr").length
+  if rows.respond_to? :should
+    rows.should==total
+  else
+    assert rows==total
+  end
 end
